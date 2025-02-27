474f67313710e438dbce76006464987e736c9634
fathinmuh <fathinmuh1@gmail.com>
2025-02-27 11:05:35 +0700

    day4.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,23 +1,54 @@
-﻿using System;
-
-struct Datadiri
-{
-    public string Nama, TTL;
-    public int NIK;
-    public Datadiri(string nama, int nik, string ttl)
-    {
-        Nama = nama;
-        NIK = nik;
-        TTL = ttl;
-    }
+﻿﻿class MyOwnCollection {
+	public int[] myCollection = new int[5];
+	public int count = 0;
+	public void Add(int input) {
+		if(count == myCollection.Length) 
+		{
+			return;
+		}
+		myCollection[count] = input;
+		count++;
+	}
+	public int Get(int index) {
+		return myCollection[index];
+	}
+	public void Remove(int index) {
+		myCollection[index] = default;
+	}
 }
-
-class Program
-{
-    static void Main()
-    {
-        // Showhow person = new Showhow("turin", 123456789, "hitlum 4 desember 4012");
-		Datadiri person = new Datadiri("turin", 123456789, "hitlum 4 desember 4012");
-		Console.WriteLine($"Nama: {person.Nama}, NIK: {person.NIK}, TTL: {person.TTL}");
-    }
+class MyOwnCollectionString {
+	public string[] myCollection = new string[5];
+	public int count = 0;
+	public void Add(string input) {
+		if(count == myCollection.Length) 
+		{
+			return;
+		}
+		myCollection[count] = input;
+		count++;
+	}
+	public string Get(int index) {
+		return myCollection[index];
+	}
+	public void Remove(int index) {
+		myCollection[index] = default;
+	}
+}
+class MyOwnCollectionFloat {
+	public float[] myCollection = new float[5];
+	public int count = 0;
+	public void Add(float input) {
+		if(count == myCollection.Length) 
+		{
+			return;
+		}
+		myCollection[count] = input;
+		count++;
+	}
+	public float Get(int index) {
+		return myCollection[index];
+	}
+	public void Remove(int index) {
+		myCollection[index] = default;
+	}
 }
