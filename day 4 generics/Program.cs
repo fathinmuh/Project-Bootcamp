33b83df9231ae9c9339f46299788227735c6c97b
fathinmuh <fathinmuh1@gmail.com>
2025-02-27 14:14:36 +0700

    generics

---
+++ day 4 generics/Program.cs
@@ -0,0 +1,32 @@
+﻿﻿using System.Numerics;
+
+class Calculator
+{
+	public T Add<T>(T a, T b)  where T : INumber<T> 
+    {
+		return a + b;
+	}
+
+	public T Minus<T>(T a, T b)  where T : ISubtractionOperators<T, T, T> 
+    {
+		return a - b;
+	}
+
+    public T Multi<T>(T a, T b)  where T : IMultiplyOperators<T, T, T> 
+    {
+		return a * b;
+	}
+
+    public T Devide<T>(T a, T b)  where T : IDivisionOperators<T, T, T> 
+    {
+		return a / b;
+	}
+
+}
+class Program {
+	static void Main() {
+		Calculator calc = new();
+        Console.WriteLine(calc.Add<int>(3,4));
+        Console.WriteLine(calc.Devide<decimal>(3,4));
+	}	
+}
