9ba661d4d2400b5853b5bb065e5dbb4d48aa8267
fathinmuh <fathinmuh1@gmail.com>
2025-02-28 14:55:13 +0700

    nullable

---
+++ nullable/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program{
+    static void Main(){
+        int? x = 5;
+        int? y = 10;
+        bool b = (x.HasValue && y.HasValue) ? (x.Value < y.Value) : false;
+        Console.WriteLine(b);
+
+        Console.WriteLine(x==5);
+        Console.WriteLine(x+y);
+    }
+}
