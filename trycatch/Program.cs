919d0a2e56b15f0f33ba367b2988f85e707053ca
fathinmuh <fathinmuh1@gmail.com>
2025-02-28 14:21:15 +0700

    tryxxx pattern

---
+++ trycatch/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+class Program{
+    static void Main()    {
+    bool TryDivide(int numerator, int denominator, out int result){
+        if (denominator == 0){
+            result = 0;
+            return false;  // Return false if division by zero
+        }
+
+        result = numerator / denominator;
+        return true;
+    }
+
+
+
+        int result;
+        bool success = TryDivide(10, 0, out result);
+
+        if (success)
+        {
+            Console.WriteLine($"Division result: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Division by zero occurred.");
+        }
+    }
+}
