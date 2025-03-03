423e74e202aa2c384c3bf98502a914f53bfa404e
fathinmuh <fathinmuh1@gmail.com>
2025-03-03 11:14:53 +0700

    foobaz

---
+++ foobaz/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+IEnumerable<int> Fibs(int fibCount)
+{
+    int prevFib = 1, curFib = 1;
+    for (int i = 0; i <= fibCount; i++)
+    {
+        yield return prevFib;
+        int newFib = prevFib + curFib;
+        prevFib = newFib;
+        // curFib = newFib;
+    }
+}
+
+string n = Console.ReadLine();
+int x= int.Parse(n);
+
+
+foreach (int fib in Fibs(x))
+{
+    Console.Write(fib + " ");
+}
+
+// for (int i = 1; i < x; i++)
+// {
+//     if (((i % 3) == 0)&&((i % 5)==0 )){
+//     Console.Write("foobar, ");
+//     }
+//     else if ((i % 3) == 0){
+//     Console.Write("foo, ");
+//     }
+//     else if ((i % 5) == 0){
+//     Console.Write("bar, ");
+//     }
+//     else Console.Write(i+", ");
+// }
+
+
