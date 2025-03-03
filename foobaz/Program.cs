a26cd6c55c41c28bc19d4f683464386ed7b45c51
fathinmuh <fathinmuh1@gmail.com>
2025-03-03 13:46:49 +0700

    foobaz 4

--- foobaz/Program.cs
+++ foobaz/Program.cs
@@ -1,30 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-string n = Console.ReadLine();
-int x= int.Parse(n);
-
-
-for (int i = 1; i < x; i++)
+﻿
+public delegate void Foobarjazz(int i);
+public class DeretBilangan
 {
-    if (((i % 7) == 0)&&((i % 5) == 0)&&((i % 3)==0 )){
-    Console.Write("foobarjazz, ");
+    public event Foobarjazz Fbj;
+    public void PrintNumbers(int n)    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
+                Kelipatan357(i);
+            } else
+            Console.Write(i + " ");
+        }
     }
-    if (((i % 7) == 0)&&((i % 5)==0 )){
-    Console.Write("barjazz, ");
+
+    public void Kelipatan357(int i){ //trigger
+        Fbj?.Invoke(i);
     }
-    if (((i % 7) == 0)&&((i % 3)==0 )){
-    Console.Write("foojazz, ");
-    }
-    if (((i % 3) == 0)&&((i % 5)==0 )){
-    Console.Write("foobar, ");
-    }
-    else if ((i % 3) == 0){
-    Console.Write("foo, ");
-    }
-    else if ((i % 5) == 0){
-    Console.Write("bar, ");
-    }
-    else Console.Write(i+", ");
 }
 
+public class Kelipatan{
+    public void Tiga(int i){
+        if (i % 3 == 0) Console.Write("Foo");
+    }
+    public void Lima(int i){
+        if (i % 5 == 0) Console.Write("Bar");
+    }
+    public void Tujuh(int i){
+        if (i % 7 == 0) Console.Write("Jazz");
+    }
+    public void Spasi(int i){
+        Console.Write(" ");
+    }
+}
 
+class Program{
+    static void Main(){
+        DeretBilangan deretBilangan = new();
+        Kelipatan kelipatan = new ();
+        string x = Console.ReadLine();
+        int y = int.Parse(x);
 
+        deretBilangan.Fbj += kelipatan.Tiga;
+        deretBilangan.Fbj += kelipatan.Lima;
+        deretBilangan.Fbj += kelipatan.Tujuh;
+        deretBilangan.Fbj += kelipatan.Spasi;
+
+        deretBilangan.PrintNumbers(y);
+    }
+}
