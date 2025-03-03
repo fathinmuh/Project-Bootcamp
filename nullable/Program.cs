e461a00645f1b29bd754db2ce3091e7295f7d36f
fathinmuh <fathinmuh1@gmail.com>
2025-03-03 10:09:36 +0700

    nullable 2

--- nullable/Program.cs
+++ nullable/Program.cs
@@ -2,10 +2,26 @@
     static void Main(){
         int? x = 5;
         int? y = 10;
-        bool b = (x.HasValue && y.HasValue) ? (x.Value < y.Value) : false;
-        Console.WriteLine(b);
+        bool V = (x.HasValue && y.HasValue) ? (x.Value < y.Value) : false;
+        Console.WriteLine(V);
 
-        Console.WriteLine(x==5);
-        Console.WriteLine(x+y);
+        Console.WriteLine("x==5 " +(x==5));
+        Console.WriteLine("x+y=" +(x+y));
+
+        if (x.HasValue)
+        {
+            int z = (int)x;
+            Console.WriteLine(z);
+        }
+        
+        object o = x;
+        Console.WriteLine("nilai o = " +o);
+
+        int? x1 = null;
+        int y1 = x1 ?? 8;  // If x is null, y will be 5
+        Console.WriteLine("kalau x1 null y1=8; " +y1);  // Output: 5
+
+        int? a = null, b = 1, c = 2;
+        Console.WriteLine(a ?? b ?? c);  // Output: 1 (first non-null value)
     }
 }
--- nullable/obj/Debug/net8.0/nullable.AssemblyInfo.cs
+++ nullable/obj/Debug/net8.0/nullable.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nullable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ba661d4d2400b5853b5bb065e5dbb4d48aa8267")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cee0d73517b063c18d11a8ba5d5b5c513a08edd")]
 [assembly: System.Reflection.AssemblyProductAttribute("nullable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nullable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
